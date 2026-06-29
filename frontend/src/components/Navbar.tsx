9509210ed53c9e4923f054b355a0bcdcfb2961d2
Livia <liv.baldochi@gmail.com>
2026-06-29 12:09:38 -0300

    Adiciona efeito de transparencia na navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,28 +1,41 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const value = Math.min(window.scrollY / 250, 1);
+      setScroll(value);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    onScroll();
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  console.log(scroll);
+
   return (
-    <header className="navbar">
+    <header
+      className="navbar"
+      style={
+        {
+          "--scroll": scroll,
+        } as React.CSSProperties
+      }
+    >
       <div className="logo">
         Centro Cultural
       </div>
 
       <nav>
-        <Link to="/">
-          Início
-        </Link>
-
-        <Link to="/programacao">
-          Programação
-        </Link>
-
-        <Link to="/exposicoes">
-          Exposições
-        </Link>
-
-        <Link to="/acervo">
-          Acervo
-        </Link>
+        <Link to="/">Início</Link>
+        <Link to="/programacao">Programação</Link>
+        <Link to="/exposicoes">Exposições</Link>
+        <Link to="/acervo">Acervo</Link>
       </nav>
     </header>
   );
